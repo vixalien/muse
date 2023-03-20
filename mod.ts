3866c72e5db202e5f4cd803d2ccda9136fa643db
Angelo Verlain <geoangercola@gmail.com>
2023-03-20 13:37:15 +0200

    export playlist in mod

--- mod.ts
+++ mod.ts
@@ -3,5 +3,6 @@ export * from "./setup.ts";
 export * from "./mixins/browsing.ts";
 export * from "./mixins/explore.ts";
 export * from "./mixins/library.ts";
+export * from "./mixins/playlist.ts";
 export * from "./mixins/queue.ts";
 export * from "./mixins/search.ts";
