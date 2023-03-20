5bc2b80cd21083458dd52fa8dfc5540e6f6d9bce
Angelo Verlain <geoangercola@gmail.com>
2023-03-20 13:36:04 +0200

    update deno std version

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { JSONPath } from "https://esm.sh/jsonpath-plus@7.2.0";
 
-export { exists } from "https://deno.land/std@0.178.0/fs/mod.ts";
+export { exists } from "https://deno.land/std@0.180.0/fs/mod.ts";
