06663e1da1f3bb39ecd8e00fc981b2c5d04f4cfa
Angelo Verlain <geoangercola@gmail.com>
2023-07-26 18:18:04 +0200

    fix previous error but for singles

--- client.ts
+++ client.ts
@@ -1,4 +1,4 @@
-import { get_explore, get_option, setup } from "./mod.ts";
+import { get_artist, get_explore, get_option, setup } from "./mod.ts";
 import { FetchClient, RequestInit } from "./request.ts";
 import { DenoFileStore } from "./store.ts";
 import { debug } from "./util.ts";
@@ -119,7 +119,7 @@ auth.addEventListener("requires-login", (event) => {
 //     console.log(await data.text());
 //   });
 
-get_explore()
+get_artist("UClYV6hHlupm_S_ObS1W-DYw")
   // get_playlist("PLCwfwQhurMOukOqbFmYRidZ81ng_2iSUE")
   // .then((data) => {
   //   return get_queue(null, data.playlistId, { autoplay: true });
