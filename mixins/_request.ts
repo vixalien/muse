15d55fb5c216225f91a95bd88e4c9da4a89ad571
Angelo Verlain <geoangercola@gmail.com>
2024-05-25 22:33:58 +0200

    chore: use `with { type: json }` instead of assert

--- mixins/_request.ts
+++ mixins/_request.ts
@@ -1,4 +1,4 @@
-import CONSTANTS2 from "../constants-ng.json" assert { type: "json" };
+import CONSTANTS from "../constants-ng.json" with { type: "json" };
 
 import { get_option, set_option } from "../setup.ts";
 
@@ -14,11 +14,11 @@ export function get_auth_headers() {
 async function load_visitor_id(signal?: AbortSignal) {
   if (!get_option("auth").has_token() && !get_option("visitor_id")) {
     const visitor_id = await get_option("client").request(
-      `${CONSTANTS2.API_URL}/browse`,
+      `${CONSTANTS.API_URL}/browse`,
       {
         method: "post",
         data: {
-          ...CONSTANTS2.DATA,
+          ...CONSTANTS.DATA,
         },
         signal,
       },
@@ -37,18 +37,18 @@ export async function request(endpoint: string, options: RequestInit) {
 
   const url = endpoint.startsWith("http")
     ? endpoint
-    : `${CONSTANTS2.API_URL}/${endpoint}`;
+    : `${CONSTANTS.API_URL}/${endpoint}`;
 
   const response = await get_option("client").request(
     use_proxy(url),
     {
       method: options.method || "post",
       data: options.method === "get" ? undefined : {
-        ...CONSTANTS2.DATA,
+        ...CONSTANTS.DATA,
         ...options.data,
       },
       headers: {
-        ...CONSTANTS2.HEADERS,
+        ...CONSTANTS.HEADERS,
         ...auth_headers,
         "Content-Type": "application/json",
         "X-Goog-Request-Time": (new Date()).getTime().toString(),
