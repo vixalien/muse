d218c4d3270e224f8db110936e57e5b03331a47b
Angelo Verlain <geoangercola@gmail.com>
2024-05-25 22:04:44 +0200

    docs: document QueueTrack

--- parsers/queue.ts
+++ parsers/queue.ts
@@ -53,16 +53,52 @@ export function parse_queue_playlist(results: any) {
   return tracks;
 }
 
+/**
+ * An object showing information about a track to be represented in a queue
+ */
 export interface QueueTrack extends SongRuns {
+  /**
+   * The video ID of the track
+   */
   videoId: string;
+  /**
+   * The name of the track
+   */
   title: string;
+  /**
+   * The duration (in words) of the track, for example "1:34"
+   */
   duration: string | null;
+  /**
+   * The duration in seconds of the track, parsed from `duration`
+   */
   duration_seconds: number | null;
+  /**
+   * The thumbnails of the track
+   */
   thumbnails: Thumbnail[];
+  /**
+   * Feedback tokens to use while adding/removing this track from the library
+   */
   feedbackTokens: MenuTokens | null;
+  /**
+   * Whether or not this track is liked/disliked by the listener
+   */
   likeStatus: LikeStatus | null;
+  /**
+   * The metadata type of this track
+   */
   videoType: VideoType | null;
+  /**
+   * Whether this track is explicit or not
+   */
   isExplicit: boolean;
+  /**
+   * The counterpart of the track.
+   * 
+   * If this track is a video, the counterpart refers to the audio-only version
+   * of the song or vice versa.
+   */
   counterpart: QueueTrack | null;
 }
 
