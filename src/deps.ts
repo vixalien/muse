56248ded85f6aeed5fae3e387d563deddad6d752
Angelo Verlain <geoangercola@gmail.com>
2024-11-12 19:50:38 +0200

    chore: update jsonpath-plus

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
-export { JSONPath, type JSONPathOptions } from "npm:jsonpath-plus@10.0.0";
+export { JSONPath, type JSONPathOptions } from "npm:jsonpath-plus@10.1.0";
 export { omit } from "npm:lodash-es@4.17.21";
 export { basename, extname } from "jsr:@std/path";
